a976c98735f16a715f55a98ae528a08133da609c
Grzegorz Pawlik <pawlik.gregor@gmail.com>
2019-02-09 16:25:53 +0000

    Create Modal component

--- src/screens/about/index.js
+++ src/screens/about/index.js
@@ -1,9 +1,8 @@
 // @flow
 import * as React from 'react';
 
-import * as routes from '~/constants/routes';
 import { navigate } from '~/navigation';
-import { Screen } from '~/components/screen';
+import { Modal } from '~/components/modal';
 import { Button } from '~/components/button';
 import { messages } from '~/domains/about/intl';
 
@@ -12,16 +11,11 @@ type Props = {};
 export class About extends React.PureComponent<Props> {
     handleOnPress = () => navigate.dismissModal();
 
-    handleShowScreen = () => navigate.push(routes.another);
-
-    handleBack = () => navigate.dismissModal();
-
     render() {
         return (
-            <Screen title={messages.title} backAction={this.handleBack} isModal>
+            <Modal title={messages.title}>
                 <Button message={messages.buttonBack} onPress={this.handleOnPress} />
-                <Button message={messages.buttonAnother} onPress={this.handleShowScreen} />
-            </Screen>
+            </Modal>
         );
     }
 }
--- src/screens/filters/component.js
+++ src/screens/filters/component.js
@@ -1,8 +1,7 @@
 // @flow
 import * as React from 'react';
 
-import { navigate } from '~/navigation';
-import { Screen } from '~/components/screen';
+import { Modal } from '~/components/modal';
 import { Button } from '~/components/button';
 import { Text } from '~/components/text';
 
@@ -16,17 +15,15 @@ export class FiltersComponent extends React.PureComponent<Props> {
         this.props.selectFilter();
     };
 
-    handleBack = () => navigate.dismissModal();
-
     render() {
         return (
-            <Screen title={messages.title} backAction={this.handleBack} isModal>
+            <Modal title={messages.title}>
                 <Text message={messages.categoryTitle} />
                 <Text message={messages.timeTitle} />
                 <Text message={messages.locationTitle} />
 
                 <Button message={messages.buttonSave} onPress={this.handleOnPress} />
-            </Screen>
+            </Modal>
         );
     }
 }

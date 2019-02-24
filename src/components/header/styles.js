7b417730c1e8d4c538627a66409c8c82255ac812
Grzegorz Pawlik <pawlik.gregor@gmail.com>
2019-02-24 17:55:33 +0000

    UI improvement header

--- src/components/header/styles.js
+++ src/components/header/styles.js
@@ -7,7 +7,12 @@ export const Container = styled.View`
     height: 50;
     align-self: stretch;
     align-items: center;
+    justify-content: space-between;
     flex-direction: row;
     border-bottom-width: 1;
     border-bottom-color: ${selectors.chalk};
+`;
+
+export const SideBox = styled.View`
+    width: 80;
 `;

d590d8390457db641aec6234aff4635616ef5b27
Grzegorz Pawlik <pawlik.gregor@gmail.com>
2019-03-02 08:33:18 +0000

    Move Like component

--- src/screens/event-details/component.js
+++ src/screens/event-details/component.js
@@ -4,31 +4,20 @@ import { Linking, TouchableOpacity } from 'react-native';
 
 import { Screen } from '~/components/screen';
 import { DateBox } from '~/components/date-box';
-import { generalIcons } from '~/constants/icons/general';
-import { SVGIcon } from '~/icons';
-import { selectors } from '~/theme/main';
 
 import { messages } from '~/domains/events/intl';
 
+import { Like } from './components/like';
 import { CoverImage, ContentBox, Title, MetaBox, MetaContent, MetaText, ContentText } from './styles';
 import type { Props } from './types';
 
 export class EventDetailsComponent extends React.PureComponent<Props> {
-    state = {
-        isLiked: false,
-    };
-
     openOrganizerUrl = () => Linking.openURL(this.props.organizerUrl);
 
     openEventUrl = () => Linking.openURL(this.props.eventUrl);
 
-    handleToggleLike = () => this.setState(state => ({ isLiked: !state.isLiked }));
-
     render() {
         const { title, location, eventStart, organizer, imageUrl, content } = this.props;
-        const { isLiked } = this.state;
-        const stroke = isLiked ? selectors.red : selectors.slate;
-        const colour = isLiked ? selectors.red : selectors.white;
 
         return (
             <Screen title={messages.eventDetailsTitle} hasContentScroll isFullWidth>
@@ -46,9 +35,7 @@ export class EventDetailsComponent extends React.PureComponent<Props> {
                         </TouchableOpacity>
                     </MetaContent>
 
-                    <TouchableOpacity onPress={this.handleToggleLike}>
-                        <SVGIcon type={generalIcons.HEART} size={30} stroke={stroke} colour={colour} />
-                    </TouchableOpacity>
+                    <Like />
                 </MetaBox>
 
                 <TouchableOpacity onPress={this.openEventUrl}>

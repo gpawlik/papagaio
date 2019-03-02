7e9cbb292f98656323d1a9159ac3b31cb995b8d1
Grzegorz Pawlik <pawlik.gregor@gmail.com>
2019-03-02 12:24:54 +0000

    About screen content

--- src/screens/about/index.js
+++ src/screens/about/index.js
@@ -1,20 +1,42 @@
 // @flow
 import * as React from 'react';
 
-import { navigate } from '~/navigation';
 import { Screen } from '~/components/screen';
-import { TextRegular2 } from '~/components/text';
+
 import { messages } from '~/domains/about/intl';
+import { generalIcons } from '~/constants/icons/general';
+import { SVGIcon } from '~/icons';
+
+import { HeaderBox, ContentBox, ContentText } from './styles';
 
 type Props = {};
 
 export class About extends React.PureComponent<Props> {
-    handleOnPress = () => navigate.dismissModal();
-
     render() {
         return (
             <Screen title={messages.title} qaName="about">
-                <TextRegular2 message="About screen" />
+                <HeaderBox>
+                    <SVGIcon type={generalIcons.LOGO} size={120} />
+                </HeaderBox>
+
+                <ContentBox>
+                    <ContentText>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer vel tortor ac odio ornare
+                        rutrum. Ut nec enim id elit tincidunt aliquet. Vestibulum ut dui quis leo vulputate efficitur
+                        vitae in est.
+                    </ContentText>
+
+                    <ContentText>
+                        Donec porta lorem augue, sit amet accumsan dolor fermentum vitae. Morbi vitae luctus nulla. In
+                        mollis viverra tempor. In hac habitasse platea dictumst. Ut porta, orci nec scelerisque
+                        pharetra, lorem sapien finibus massa, non lobortis sapien lectus lobortis nunc.
+                    </ContentText>
+
+                    <ContentText>
+                        Nullam accumsan velit a turpis feugiat, at auctor sapien tincidunt. In pretium enim id aliquet
+                        tincidunt.
+                    </ContentText>
+                </ContentBox>
             </Screen>
         );
     }
---
+++ src/screens/about/styles.js
@@ -0,0 +1,17 @@
+// @flow
+import styled from 'styled-components/native';
+
+import { TextRegular3 } from '~/components/text';
+
+import { selectors } from '~/theme/main';
+
+export const HeaderBox = styled.View`
+    margin-vertical: 30px;
+    align-items: center;
+`;
+
+export const ContentBox = styled.View``;
+
+export const ContentText = styled(TextRegular3)`
+    margin-bottom: ${selectors.ss6};
+`;

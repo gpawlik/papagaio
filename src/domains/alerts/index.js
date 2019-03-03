ec0c38eca97460e844fa2a7d4f075c19e402e926
Grzegorz Pawlik <pawlik.gregor@gmail.com>
2019-03-03 18:58:01 +0000

    Add events and reducers for alerts

--- src/domains/alerts/index.js
+++ src/domains/alerts/index.js
@@ -2,16 +2,19 @@
 import { handleActions } from 'redux-actions';
 import { Map as ImmutableMap } from 'immutable';
 
+import { handleReduce } from '~/utils/handle-reduce';
+
+import { setAlert, resetAlerts } from './actions';
+
 type State = ImmutableMap<string, *>;
 
 export const initialState: State = ImmutableMap({
-    alert: ImmutableMap({
-        id: 1,
-        message: 'You are so offline.',
-        type: 'ERROR',
-    }),
+    alert: ImmutableMap(),
 });
 
-const actionHandlers = new Map([]);
+const actionHandlers = new Map([
+    [setAlert, handleReduce((state, { payload }) => state.set('alert', ImmutableMap(payload)))],
+    [resetAlerts, handleReduce(state => state.set('alert', ImmutableMap()))],
+]);
 
 export const alertsReducer = handleActions(actionHandlers, initialState);
